efaaaa935b14562a3a4bad8ef257d00c539e2aa6
Ricardo Gorki <ricardo.gorki@gmail.com>
2024-01-11 00:59:04 -0300

    index users padrao

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+	<title>Leader-Test</title>
+</head>
+
+<body>
+	<header class="header-menu">
+		<img id="header-logo" src="php-elephant.svg" alt="php">
+		<nav>
+			<ul class="menu">
+				<li><a>Colaborador</a>
+					<ul>
+						<li><a href="#">Cadastrar</a></li>
+						<li><a href="#">Listar</a></li>
+						<li><a href="#">Atualizar</a></li>
+						<li><a href="#">Deleter</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<section>
+
+	</section>
+</body>
+
+</html>
