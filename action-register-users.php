35485a8679b3a8a178ce187f267b8e603ac592ad
Ricardo Gorki <ricardo.gorki@gmail.com>
2024-01-12 02:17:17 -0300

    record, but need alter getters in Values

--- action-register-users.php
+++ action-register-users.php
@@ -1,6 +1,5 @@
 <?php
-require ("src/entities/User.php");
-require ("src/lib/myDb.php");
+require("src/entities/User.php");
 
 $name = $_POST['name'];
 $lastName = $_POST['lastName'];
@@ -9,9 +8,29 @@ $phone =  $_POST['phone'];
 $birthDay = $_POST['birthDay'];
 
 $user = new User($name, $lastName, $email, $phone, $birthDay);
-/*
-$sql = "INSERT INTO users (id, name, lastname, $email, $phone, $birthDay) VALUES (
-	'$user->getId()', '$user->getName()', '$user->getLastName()', '$user->getEmail()', '$user->getPhone()', '$user->getBirthDay()')";
-	 */
 
+$newDate = strtotime($user->getBirthDay());
+echo $formatedBirthDay = date('Y-m-d H:i:s', $newDate);
+
+$servername = "localhost";
+$database = "leader";
+$username = "leader";
+$password = "root";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+
+if (isset($name)) {
+	echo "Connected successfully";
+	$sql = "INSERT INTO users(id, name, last_name, email, phone_numbers, birth_day) VALUES('$user->getId()', '$user->$getName', '$user->$getLastName', '$user->$getEmail', '$user->$getPhone', '$formatedBirthDay')";
+}
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+}
+mysqli_close($conn);
 ?>
