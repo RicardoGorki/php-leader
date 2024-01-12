e8838a99f1acb9452a3abff4bab8074a5d90ee2b
Ricardo Gorki <ricardo.gorki@gmail.com>
2024-01-12 05:19:13 -0300

    list and style

--- list-users.php
+++ list-users.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="src/styles/styles.css">
+	<link rel="stylesheet" href="src/styles/styles-list-users.css">
 </head>
+
 <body>
 	<div>
-		<?php include 'src/components/menu.php';?>
+		<?php include 'src/components/menu.php'; ?>
 	</div>
-	<section>
+	<section class="list-body">
+		<?php
 
+		$conn = new mysqli("localhost", "leader", "root", "leader");
+
+		if (!$conn) {
+			die("Connection failed: " . mysqli_error($conn));
+		}
+		$query = $conn->query("SELECT * FROM users ORDER by name");
+		while ($row = $query->fetch_array()) {
+			echo "<section class='container-list'>";
+
+			echo "<label class='list-label'>"."Nome: "."</label>"."<div class='list-item'>".  $row['name'] . "</div>";
+
+			echo "<label class='list-label'>"."Sobrenome: "."</label>"."<div class='list-item'>".  $row['last_name'] . "</div>";
+			echo "<label class='list-label'>"."Email: "."</label>". "<div class='list-item'>".  $row['email'] . "</div>";
+			echo "<label class='list-label'>"."Tel: "."</label>". "<div class='list-item'>".  $row['phone_numbers'] . "</div>";
+			echo "<label class='list-label'>"."Data de Nascimento: ". "</label>" ."<div class='list-item'>". $row['birth_day'] . "</div>";
+			echo "</section>";
+		}
+		?>
 	</section>
 </body>
 
