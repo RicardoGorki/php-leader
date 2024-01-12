2abd5ed806b390fbfc41b95e209a69a33c61a4f6
Ricardo Gorki <ricardo.gorki@gmail.com>
2024-01-12 13:39:01 -0300

    deleting but need fix

--- list-users.php
+++ list-users.php
@@ -19,19 +19,31 @@
 		if (!$conn) {
 			die("Connection failed: " . mysqli_error($conn));
 		}
-		$query = $conn->query("SELECT * FROM users ORDER by name");
-		while ($row = $query->fetch_array()) {
+		$sql = $conn->query("SELECT * FROM users ORDER by name");
+		while ($row = $sql->fetch_array()) {
 			echo "<section class='container-list'>";
 
-			echo "<label class='list-label'>"."Nome: "."</label>"."<div class='list-item'>".  $row['name'] . "</div>";
+			echo "<label class='list-label'>" . "Nome: " . "</label>" . "<div class='list-item'>" .  $row['name'] . "</div>";
 
-			echo "<label class='list-label'>"."Sobrenome: "."</label>"."<div class='list-item'>".  $row['last_name'] . "</div>";
-			echo "<label class='list-label'>"."Email: "."</label>". "<div class='list-item'>".  $row['email'] . "</div>";
-			echo "<label class='list-label'>"."Tel: "."</label>". "<div class='list-item'>".  $row['phone_numbers'] . "</div>";
-			echo "<label class='list-label'>"."Data de Nascimento: ". "</label>" ."<div class='list-item'>". $row['birth_day'] . "</div>";
+			echo "<label class='list-label'>" . "Sobrenome: " . "</label>" . "<div class='list-item'>" .  $row['last_name'] . "</div>";
+			echo "<label class='list-label'>" . "Email: " . "</label>" . "<div class='list-item'>" .  $row['email'] . "</div>";
+			echo "<label class='list-label'>" . "Tel: " . "</label>" . "<div class='list-item'>" .  $row['phone_numbers'] . "</div>";
+			echo "<label class='list-label'>" . "Data de Nascimento: " . "</label>" . "<div class='list-item'>" . $row['birth_day'] . "</div>";
+			echo "<a value='Deletar'>";
+			echo $row['id'];
+			if (isset($_GET['submitdelete'])) {
+				$action_id = $row['id'];
+				$del = $conn->query("DELETE FROM users WHERE id='$action_id'");
+			}
+			"</input>";
+			"</form>";
+
 			echo "</section>";
+
 		}
+		mysqli_close($conn);
 		?>
+
 	</section>
 </body>
 
