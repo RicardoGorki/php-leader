e6b87c740cad27731d7ea1c08b297346ff63845f
Ricardo Gorki <ricardo.gorki@gmail.com>
2024-01-14 16:42:22 -0300

    index

--- index.php
+++ index.php
@@ -8,16 +8,17 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
-	<title>Leader-Test</title>
 </head>
 <?php require_once 'src/components/header.php';
 			require_once 'src/components/footer.php';
 			?>
-
 <body>
-
-
+<div class="avisos">
+<h1>Bem Vindo!</h1>
+<h4>Seed</h4>
+<p>Popula o banco com 50 usuarios</p>
+<h4>Apagar Tudo</h4>
+<p>Deleta somente os usuarios, não deleta a tabela.</p>
+</div>
 </body>
-
-
 </html>
