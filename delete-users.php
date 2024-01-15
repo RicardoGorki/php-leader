275ac923298242af3eddab97bd0afc3d0f099815
Ricardo Gorki <ricardo.gorki@gmail.com>
2024-01-15 01:30:54 -0300

    style delete page

--- delete-users.php
+++ delete-users.php
@@ -1,42 +1,22 @@
 <?php
-			require_once ("src/database/dbconnect.php");
-			require_once ("src/database/database.php");
-			require_once ("src/entities/User.php");
+require_once("src/database/dbconnect.php");
+require_once("src/database/database.php");
+require_once("src/entities/User.php");
+require_once 'src/components/header.php';
+require_once 'src/components/footer.php';
 
-			if(isset($_POST['id']))
-				deleteUserAction($conn, $_POST['id']);
+if (isset($_POST['id']))
+	deleteUserAction($conn, $_POST['id']);
 
-		?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="src/styles/styles.css">
-</head>
-<body>
-		<?php require_once 'src/components/header.php';
-			require_once 'src/components/footer.php';
-		?>
-	<section>
-
-	<div class="container">
-    <div class="row">
-        <a href="../../../index.php"><h1>Users - Delete</h1></a>
-        <a class="btn btn-success text-white" href="list-users.php">Prev</a>
-    </div>
-    <div class="row flex-center">
-        <div class="form-div">
-            <form class="form" action="delete-users.php" method="POST">
-                <label>Do you really want to remove the user?</label>
-                <input type="hidden" name="id" value="<?=$_GET['id']?>" required/>
-
-                <button class="btn btn-success text-white" type="submit">Yes</button>
-            </form>
-        </div>
-    </div>
+?>
+<link rel="stylesheet" href="src/styles/styles.css">
+<div class="form-delete">
+	<form action="delete-users.php" method="POST">
+		<label>Deseja remover este usuário?</label>
+		<div class="div-delete">
+		<a class="prev-btn" href="list-users.php">Voltar</a>
+		<input type="hidden" name="id" value="<?= $_GET['id'] ?>" required />
+		<button class="btn-delete" type="submit">Deletar</button>
+		<div>
+	</form>
 </div>
-	</section>
-</body>
-
-</html>
